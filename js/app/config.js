3e43d6627f93f0abdffdfb54d40208a94cc23a9f
clorange <lorange.christian@gmail.com>
2018-04-18 09:07:16 -0600

    no message

--- js/app/config.js
+++ js/app/config.js
@@ -1,17 +1,26 @@
-Copyright (c) 2012 Adobe Systems Incorporated. All rights reserved.
+/*
+Copyright (c) 2013 Adobe Systems Incorporated. All rights reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
+*/
+define(function(require, exports, module) {
 
-Please note that some portions of this project are written by third parties
-under different license terms. Your use of those portions are governed by
-the license terms contained in the corresponding files.
+    var config = {
+        //Local files
+        url: 'data/',
+        //Remote urls
+        //url: "https://cssconf.attendease.com/api/"
+    };
+
+    return config;
+});
