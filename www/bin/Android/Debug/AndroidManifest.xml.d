324d04ff02f989721b6d01425c9e78f6c3c17f11
clorange <lorange.christian@gmail.com>
2018-04-18 07:49:51 -0600

    no message

--- www/bin/Android/Debug/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\Users\sbrand\OneDrive\Projects\CampusDays2014\CampusDays2014\bld\Debug\platforms\android\ant-build\AndroidManifest.xml : \
-C:\Users\sbrand\OneDrive\Projects\CampusDays2014\CampusDays2014\bld\Debug\platforms\android\AndroidManifest.xml \
--- www/bin/Android/Release/CordovaApp-release-unsigned.apk.d
+++
@@ -1,4 +0,0 @@
-C:\Users\sbrand\OneDrive\Projects\CampusDays2014\CampusDays2014\bld\Release\platforms\android\ant-build\CordovaApp-release-unsigned.apk : \
-C:\Users\sbrand\OneDrive\Projects\CampusDays2014\CampusDays2014\bld\Release\platforms\android\ant-build\CordovaApp.ap_ \
-C:\Users\sbrand\OneDrive\Projects\CampusDays2014\CampusDays2014\bld\Release\platforms\android\ant-build\classes.dex \
-C:\Users\sbrand\OneDrive\Projects\CampusDays2014\CampusDays2014\bld\Release\platforms\android\CordovaLib\ant-build\classes.jar \
