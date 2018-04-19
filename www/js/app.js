3ab87ee8f76b01ab14f38ffbc90e5afbb0df3328
DESKTOP-R7E62OP\Christian L'Orange <lorange.christian@gmail.com>
2018-04-18 20:09:30 -0600

    Functioning design (fingers crossed)

--- www/js/app.js
+++ www/js/app.js
@@ -11,7 +11,7 @@ $(function() {
    });
 
    $("#SECOND A").live("click", function(){
-       moveRow($(this).parents("tr").clone(), $("#FIRST"), "Remove from My List");
+       moveRow($(this).parents("tr").clone(), $("#FIRST"), "REMOVE");
        sortTable();
    });
    $('#FIRST A,#SECOND A').live('click', function() {
@@ -29,5 +29,78 @@ $(function() {
 
 
 
+function sortTable(){
+    var tbl = document.getElementById("SECOND").tBodies[0];
+    var store = [];
+    for(var i=0, len=tbl.rows.length; i<len; i++){
+        var row = tbl.rows[i];
+        var sortnr = parseFloat(row.cells[0].textContent || row.cells[0].innerText);
+        if(!isNaN(sortnr)) store.push([sortnr, row]);
+    }
+    store.sort(function(x,y){
+        return x[0] - y[0];
+    });
+    for(var i=0, len=store.length; i<len; i++){
+        tbl.appendChild(store[i][1]);
+    }
+    store = null;
+}
 
 
+function openModal() {
+  document.getElementById('myModal').style.display = "block";
+}
+
+function closeModal() {
+  document.getElementById('myModal').style.display = "none";
+}
+
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
+
+/* Card Layout for Table */
+
+var tables = $('.cards-table');
+
+// Create an array containing all table headers
+var table_headers = [];
+tables.each(function() {
+  var th = [];
+  $(this).find('thead th').each(function() {
+    th.push($(this).text());
+  });
+  table_headers.push(th);
+});
+
+// Add a data-label attribute to each cell with the value of the corresponding column header
+// Iterate through each table
+tables.each(function(table) {
+  var table_index = table;
+  // Iterate through each row
+  $(this).find('tbody tr').each(function() {
+    // Finally iterate through each column/cell
+    $(this).find('td').each(function(column) {
+      $(this).attr('data-label', table_headers[table_index][column]);
+    });
+  });
+});
